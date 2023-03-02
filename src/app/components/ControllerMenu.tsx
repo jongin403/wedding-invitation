70043af4cdd5376a1973b8b78ff3ba821d8d4eea
jongin403 <jongin403@naver.com>
2023-03-02 23:18:42 +0900

    feat: component 추가

---
+++ src/app/components/ControllerMenu.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+
+type Props = {};
+
+function ControllerMenu({}: Props) {
+  return (
+    <div>
+      <h1>ControllerMenu</h1>
+      <div>맨위로</div>
+      <div>각 메뉴별로 빠르게 이동할 수 있는 버튼</div>
+      <div>각 메뉴별로 빠르게 이동할 수 있는 버튼</div>
+      <div>각 메뉴별로 빠르게 이동할 수 있는 버튼</div>
+    </div>
+  );
+}
+
+export default ControllerMenu;
---
+++ src/app/components/sponsor.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+
+type Props = {};
+
+function Sponsor({}: Props) {
+  return (
+    <div>
+      <h1>Sponsor</h1>
+      <div>
+        스폰서처럼 후원해준 기업 ( 전직장, 현직장 ) 이 하단에서 slide 되고있으면
+        재미있을 것 같음
+      </div>
+    </div>
+  );
+}
+
+export default Sponsor;
