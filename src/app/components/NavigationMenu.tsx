0707ed52925926c1794e5e93e96a7f2a76de6a52
jongin403 <jongin403@naver.com>
2023-03-03 01:44:56 +0900

    build: styled-component 제거

--- src/app/components/NavigationMenu.tsx
+++ src/app/components/NavigationMenu.tsx
@@ -2,34 +2,14 @@
 
 import React from 'react';
 import Link from 'next/link';
-import styled, { css } from 'styled-components';
 
 type Props = {};
 
 function NavigationMenu({}: Props) {
-  const Button = styled.button`
-    background: transparent;
-    border-radius: 3px;
-    border: 2px solid palevioletred;
-    color: palevioletred;
-    margin: 0.5em 1em;
-    padding: 0.25em 1em;
-
-    ${(props) =>
-      props.primary &&
-      css`
-        background: palevioletred;
-        color: white;
-      `}
-  `;
   return (
     <div>
       <h1>NavigationMenu</h1>
-      <Button>Normal Button</Button>
-      <Button className="primary">Primary Button</Button>
-      <Link className="primary" href="/">
-        home
-      </Link>
+      <Link href="/">home</Link>
       <Link href="/wedding-info">wedding-info</Link>
       <Link href="/photo-gallery">photo-gallery</Link>
       <Link href="/guest-book">guest-book</Link>
