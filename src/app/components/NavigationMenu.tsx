80620d0482f0084e7f61da00999a8d3012c40827
jongin403 <jongin403@naver.com>
2023-03-05 23:56:28 +0900

    feat: emotion 추가

--- src/app/components/NavigationMenu.tsx
+++ src/app/components/NavigationMenu.tsx
@@ -5,10 +5,22 @@ import Link from 'next/link';
 
 type Props = {};
 
+import styled from '@emotion/styled';
+
+const SomeComp = styled.button({
+  background: 'hotpink',
+});
+
+const AnotherComp = styled.button`
+  background: ${(props) => props.background};
+`;
+
 function NavigationMenu({}: Props) {
   return (
     <div>
       <h1>NavigationMenu</h1>
+      <AnotherComp background="green" />
+      <SomeComp />
       <Link href="/">home</Link>
       <Link href="/wedding-info">wedding-info</Link>
       <Link href="/photo-gallery">photo-gallery</Link>
