62014f9661a719a7e98ac863831f8fcba3d84e6f
jongin403 <jongin403@naver.com>
2023-06-12 02:35:53 +0900

    feat: 홈 페이지 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import styles from './Home.module.scss';
+
 const Home = () => {
   return (
-    <div>
-      <div>Who? 형다은 & 정종인 이가</div>
-      <div>What? 결혼식 을</div>
-      <div>When? 24년 1월 13일 (토) 에</div>
-      <div>Where? 아펠가모 반포 에서</div>
+    <div className={styles.briefInfoWrap}>
+      <div className={styles.briefInfoList}>
+        <div className={styles.briefInfoItem}>
+          Who?&nbsp;
+          <span className={styles.pointWord}>형다은 & 정종인</span>
+          &nbsp;이
+        </div>
+        <div className={styles.briefInfoItem}>
+          What?&nbsp;
+          <span className={styles.pointWord}>결혼식</span>
+          &nbsp;을
+        </div>
+        <div className={styles.briefInfoItem}>
+          When?&nbsp;
+          <span className={styles.pointWord}>24년 1월 13일 (토)</span>
+          &nbsp;에
+        </div>
+        <div className={styles.briefInfoItem}>
+          Where?&nbsp;
+          <span className={styles.pointWord}>아펠가모 반포</span>
+          &nbsp;에서
+        </div>
+      </div>
     </div>
   );
 };
