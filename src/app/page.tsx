9b204c08d205eec6d1213188184e480ae0e0f95b
jongin403 <jongin403@naver.com>
2023-06-11 23:59:31 +0900

    feat: 홈 페이지 디자인 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function Home() {
+const Home = () => {
   return (
-    <>
-      <div>Home</div>
-      <div>홈에는 모바일 한 화면에 볼 수 있는 간략화된 정보만 들어갑니다.</div>
-      <div>탭하면 메뉴를 볼 수 있는 상세정보로 들어갑니다.</div>
-      <div>여기에서는 메뉴가 안보입니다.</div>
-    </>
+    <div>
+      <div>Who? 형다은 & 정종인 이가</div>
+      <div>What? 결혼식 을</div>
+      <div>When? 24년 1월 13일 (토) 에</div>
+      <div>Where? 아펠가모 반포 에서</div>
+    </div>
   );
-}
+};
 
 export default Home;
