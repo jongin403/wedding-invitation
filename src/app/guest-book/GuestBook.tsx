ad1e60aecabe28de7d815343d71c410b5311344d
jongin403 <jongin403@naver.com>
2023-06-29 23:52:47 +0900

    Revert "chore: 카카오 close test"
    This reverts commit 7d5ebf1efda5338f2d6ae8dc62e86b95f671530c.

--- src/app/guest-book/GuestBook.tsx
+++ src/app/guest-book/GuestBook.tsx
@@ -2,45 +2,10 @@
 
 import React from 'react';
 
-function ClosePage() {
-  const _ua = window.navigator.userAgent;
-  alert(_ua);
-  if (_ua.toLocaleLowerCase().indexOf('kakaotalk') > -1) {
-    alert('!');
-    window.location.href = /iPad|iPhone|iPod/.test(_ua)
-      ? 'kakaoweb://closeBrowser'
-      : 'kakaotalk://inappbrowser/close';
-  } else if (_ua.toLocaleLowerCase().indexOf('android') > -1) {
-    alert(window.location.href);
-    self.close();
-  } else if (
-    _ua.toLocaleLowerCase().indexOf('iphone') > -1 ||
-    _ua.toLocaleLowerCase().indexOf('ipad') > -1 ||
-    _ua.toLocaleLowerCase().indexOf('ipod') > -1
-  ) {
-    alert('!!!');
-    self.close();
-  } else {
-    alert('!!!!');
-    self.close();
-  }
-}
-
-const close1 = () => {
-  alert('close1');
-  window.close();
-};
-const close2 = () => {
-  self.close();
-};
-
 function GuestBook() {
   return (
     <div>
       <h1>GuestBook</h1>
-      <button onClick={close1}>닫기1</button>
-      <button onClick={close2}>닫기2</button>
-      <button onClick={ClosePage}>ClosePage</button>
     </div>
   );
 }
